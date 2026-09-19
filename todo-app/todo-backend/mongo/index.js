fbc15b076ca5010aa63d3bd08ae339ebb6e9b036
SoumyA16-git <soumyaranjan.baroda@gmail.com>
2026-09-19 22:27:58 +0530

    fix(mongo): mount mongo-init.js, set init db env, and add retry logic

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -3,7 +3,17 @@ const Todo = require('./models/Todo')
 const { MONGO_URL } = require('../util/config')
 
 if (MONGO_URL && !mongoose.connection.readyState) {
-  mongoose.connect(MONGO_URL)
+  const connectWithRetry = () => {
+    mongoose.connect(MONGO_URL)
+      .then(() => {
+        console.log('Connected to MongoDB')
+      })
+      .catch((err) => {
+        console.error('MongoDB connection error, retrying in 3 seconds...', err.message)
+        setTimeout(connectWithRetry, 3000)
+      })
+  }
+  connectWithRetry()
 }
 
 module.exports = {
